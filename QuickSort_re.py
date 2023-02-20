acd32230c3013aa17cfaf8fa9685018160331b3e
V1rru <97950906+V1rru@users.noreply.github.com>
2023-02-20 16:41:10 +0300

    .

--- QuickSort_re.py
+++ QuickSort_re.py
@@ -1,13 +1,30 @@
+amount_of_comparisons = 0
+amount_of_permutations = 0
+
 def quick_sort(n):
     
+    global amount_of_comparisons
+    global amount_of_permutations
+
     if len(n) <= 1:
         return n
 
     root = n[0]
     left = list(filter(lambda x: x < root, n))
+    
+    amount_of_comparisons += len(left)
+    amount_of_permutations += len(left)
+
     center = [i for i in n if i == root]
+
     right = list(filter(lambda x: x > root, n))
+
+    amount_of_comparisons += len(right)
 
     return quick_sort(left) + center + quick_sort(right)
 
+
 print(quick_sort([7, 6, 9, 8, 10, 4, 3, 5]))
+# print(quick_sort([1, 2, 3, 4, 5, 6, 7, 8]))
+print(amount_of_comparisons)
+print(amount_of_permutations)
